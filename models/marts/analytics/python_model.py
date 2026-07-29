892b4eb360d55ffe3f6e5b6c316e063e64052bb4
esimbia <esta.simbia@sogeti.com>
2026-07-29 09:39:51 +0200

    Adding a workflow for cleaning up PR schemas in CI database

--- models/marts/analytics/python_model.py
+++ models/marts/analytics/python_model.py
@@ -1,23 +1,23 @@
-import holidays
+# import holidays
 
 
-def is_holiday(date_col):
-    french_holiday = holidays.France()
-    is_holiday = (date_col in french_holiday)
-    return is_holiday
+# def is_holiday(date_col):
+#     french_holiday = holidays.France()
+#     is_holiday = (date_col in french_holiday)
+#     return is_holiday
 
 
-def model(dbt, session):
+# def model(dbt, session):
 
-    dbt.config(packages=['holidays'])
-    my_sql_model_df = dbt.ref("stablecoin_activity_per_day")
-    # my_sql_model_df = dbt.source("eth", "contracts")
+#     dbt.config(packages=['holidays'])
+#     my_sql_model_df = dbt.ref("stablecoin_activity_per_day")
+#     # my_sql_model_df = dbt.source("eth", "contracts")
 
-    my_sql_model_df = my_sql_model_df.to_pandas()
+#     my_sql_model_df = my_sql_model_df.to_pandas()
 
-    my_sql_model_df["is_holiday"] = my_sql_model_df["DATE"].apply(is_holiday)
+#     my_sql_model_df["is_holiday"] = my_sql_model_df["DATE"].apply(is_holiday)
 
-    # print(type(my_sql_model_df))
-    # raise Exception(type(my_sql_model_df))
+#     # print(type(my_sql_model_df))
+#     # raise Exception(type(my_sql_model_df))
 
-    return my_sql_model_df
+#     return my_sql_model_df
